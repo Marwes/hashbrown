d917e721036f81f2f2e1cbe486ccf01430ddae0b
Markus Westerlind <markus.westerlind@distilnetworks.com>
2020-09-29 12:10:37 +0200

    perf(compile): Move part of fallible_with_capacity out

--- src/raw/mod.rs
+++ src/raw/mod.rs
@@ -465,13 +465,21 @@ impl<T, A: AllocRef + Clone> RawTable<T, A> {
                     Some(buckets) => buckets,
                     None => return Err(fallability.capacity_overflow()),
                 };
-                let result = Self::new_uninitialized(alloc, buckets, fallability)?;
-                result
-                    .table
-                    .ctrl(0)
-                    .write_bytes(EMPTY, result.table.num_ctrl_bytes());
-
-                Ok(result)
+                // Avoid `Option::ok_or_else` because it bloats LLVM IR.
+                let (layout, ctrl_offset) = match calculate_layout::<T>(buckets) {
+                    Some(lco) => lco,
+                    None => return Err(fallability.capacity_overflow()),
+                };
+                Ok(Self {
+                    table: RawTableInner::fallible_with_capacity(
+                        alloc,
+                        buckets,
+                        fallability,
+                        layout,
+                        ctrl_offset,
+                    )?,
+                    marker: PhantomData,
+                })
             }
         }
     }
@@ -1130,6 +1138,21 @@ impl<A: AllocRef + Clone> RawTableInner<A> {
             growth_left: bucket_mask_to_capacity(buckets - 1),
             alloc,
         })
+    }
+
+    /// Attempts to allocate a new hash table with at least enough capacity
+    /// for inserting the given number of elements without reallocating.
+    unsafe fn fallible_with_capacity(
+        alloc: A,
+        buckets: usize,
+        fallability: Fallibility,
+        layout: Layout,
+        ctrl_offset: usize,
+    ) -> Result<Self, TryReserveError> {
+        let result = Self::new_uninitialized(alloc, buckets, fallability, layout, ctrl_offset)?;
+        result.ctrl(0).write_bytes(EMPTY, result.num_ctrl_bytes());
+
+        Ok(result)
     }
 
     /// Searches for an empty or deleted bucket which is suitable for inserting
